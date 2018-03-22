5aa452febe6d2b1f49c70c96d05a0849b3e2f293
Mad-Catter <teriomon5@aol.com>
2018-03-22 14:50:58 -0400

    Added several new songs to test, several comments, and more punctuation to pray.

--- fetch_data.py
+++ fetch_data.py
@@ -4,16 +4,21 @@ import re
 
 
 def cleaner(link, songname):
+	#The link finder through semi_clean I basically just ripped off of the Beautiful Soup webpage.
 	link_finder = urllib2.urlopen('%s' % (link))
 	link_opener = link_finder.read()
 	soup = BeautifulSoup(link_opener,'html.parser')
 	semi_clean = str(soup.get_text())
+
+	#This only works with the site azlyrics.  They always start their actual lyrics with the song name lyrics and end it with several new lines.
 	startlyric = '"%s" lyrics' % (songname)
 
 	stoplyric = """
 
 """
-	pray = re.search(r'%s[a-z|\s|A-Z|\"|\'|\-|\?|!|\,|\.|\(|\)]+%s' % (startlyric,stoplyric), semi_clean)
+	#Pray searches for the start lyric and then continously searches for any letters (a-z), any numbers (\d), any white spaces (\s), or any punctuation (everything else there) until it reaches the several new
+	#lines that signify the end of the song.
+	pray = re.search(r'%s[a-z|\s|A-Z|\d|\"|\'|\-|\?|!|\,|\.|\(|\)|\[|\]|\{|\}]+%s' % (startlyric,stoplyric), semi_clean)
 	if pray:
 		print pray.group(0)
 	else:
--- run.py
+++ run.py
@@ -4,3 +4,6 @@ from fetch_data import cleaner
 cleaner("https://www.azlyrics.com/lyrics/queen/imgoingslightlymad.html","I\'m Going Slightly Mad") 
 cleaner("https://www.azlyrics.com/lyrics/queen/dontstopmenow.html","Don\'t Stop Me Now")
 cleaner("https://www.azlyrics.com/lyrics/queen/wewillrockyou.html", "We Will Rock You")
+cleaner("https://www.azlyrics.com/lyrics/queen/sheerheartattack.html", "Sheer Heart Attack")
+cleaner("https://www.azlyrics.com/lyrics/queen/youtakemybreathaway.html", "You Take My Breath Away")
+cleaner("https://www.azlyrics.com/lyrics/queen/iwantitall.html","I Want It All")
