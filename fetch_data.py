ee69f20119a77556f46f303442a85496711cd80d
Mad-Catter <teriomon5@aol.com>
2018-03-22 13:37:56 -0400

    Pray has cleaned up most of the text!

--- fetch_data.py
+++ fetch_data.py
@@ -16,17 +16,24 @@ def cleaner(link, songname):
 
 	stoplyric = "if  \("
 
-	normie = re.compile(r"%s[.|\s]+ %s" % (startlyric, stoplyric))
+	normie = re.compile(r"%s[a-z|\s|A-Z|\n]+ %s" % (startlyric, stoplyric))
 	norm_match = normie.finditer(tomato)
 	for match in norm_match:
+		print match.span()
 		print match.group(0)
+		print match
+		print norm_match.group(0)
+		print("Is this even triggering?")
 	print norm_match
+	print 0x05BB8110
 
-	pray = re.search(r'%s' % (stoplyric), tomato)
+	pray = re.search(r'%s[a-z|\s|A-Z|\n|\"|\'|\-|\?|!|\,]+%s' % (startlyric,stoplyric), tomato)
 	if pray:
 		print pray.group(0)
+	else:
+		print("No match")
 	print normie
-	print(r"%s[.|\s]+ %s" % (startlyric, stoplyric))
+	#print(r"%s[.|\s]+ %s" % (startlyric, stoplyric))
 
 
 	pass
