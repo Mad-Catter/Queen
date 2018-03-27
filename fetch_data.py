4a46307393da8cad29ebd1a1d4aaa7f49f710274
Mad-Catter <teriomon5@aol.com>
2018-03-27 12:58:27 -0400

    Fixed Errors with certain songs by adding the line "# -*- coding: utf-8 -*-" at the beginning of fetch_data (minus the quotation marks).  There was a special character in certain songs that was messing up the encoding process(?).  Adding that to the top of the file(only works at the top) and adding the special character to the search list fixed my problems.

--- fetch_data.py
+++ fetch_data.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import urllib2
 from bs4 import BeautifulSoup
 import re
@@ -8,7 +9,7 @@ def cleaner(link, songname):
 	link_finder = urllib2.urlopen('%s' % (link))
 	link_opener = link_finder.read()
 	soup = BeautifulSoup(link_opener,'html.parser')
-	semi_clean = str(soup.get_text())
+	semi_clean = soup.get_text().encode('utf-8')
 
 	#This only works with the site azlyrics.  They always start their actual lyrics with the song name lyrics and end it with several new lines.
 	startlyric = '"%s" lyrics' % (songname)
@@ -18,7 +19,7 @@ def cleaner(link, songname):
 """
 	#Pray searches for the start lyric and then continously searches for any letters (a-z), any numbers (\d), any white spaces (\s), or any punctuation (everything else there) until it reaches the several new
 	#lines that signify the end of the song.
-	pray = re.search(r'%s[a-z|\s|A-Z|\d|\"|\'|\-|\?|!|\,|\.|\(|\)|\[|\]|\{|\}]+%s' % (startlyric,stoplyric), semi_clean)
+	pray = re.search(r'%s[a-z|\s|A-Z|\d|\"|\'|\-|\?|!|\,|\.|\(|\)|\[|\]|\{|\}|\—|ë]+%s' % (startlyric,stoplyric), semi_clean)
 	if pray:
 		print pray.group(0)
 	else:
--- run.py
+++ run.py
@@ -7,3 +7,6 @@ cleaner("https://www.azlyrics.com/lyrics/queen/wewillrockyou.html", "We Will Roc
 cleaner("https://www.azlyrics.com/lyrics/queen/sheerheartattack.html", "Sheer Heart Attack")
 cleaner("https://www.azlyrics.com/lyrics/queen/youtakemybreathaway.html", "You Take My Breath Away")
 cleaner("https://www.azlyrics.com/lyrics/queen/iwantitall.html","I Want It All")
+cleaner("https://www.azlyrics.com/lyrics/queen/theshowmustgoon.html", "The Show Must Go On")
+cleaner("https://www.azlyrics.com/lyrics/queen/killerqueen.html", "Killer Queen")
+cleaner("https://www.azlyrics.com/lyrics/queen/bohemianrhapsody.html","Bohemian Rhapsody")
