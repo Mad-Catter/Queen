1ba693f8a0c1e59a4531ac6b2ebc791191b6defe
Mad-Catter <teriomon5@aol.com>
2018-04-06 15:31:50 -0400

    Fixed problem where there would be too many lyrics.

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from markov_python.cc_markov import MarkovChain
 from fetch_data import cleaner
+#I'm importing MarkovChain from the premade code academy markov chain creator, and I'm importing my cleaner from fetch_data.
 
+#The list songs will be used for storing all of the lyrics for randomization.
 songs = []
 
+#These are all the songs I'm currently using.  Calling cleaner to get the lyrics and then putting them all into the songs list.
 songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/imgoingslightlymad.html","I\'m Going Slightly Mad")) 
 songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/dontstopmenow.html","Don\'t Stop Me Now"))
 songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/wewillrockyou.html", "We Will Rock You"))
@@ -13,13 +16,16 @@ songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/theshowmustgoon.html
 songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/killerqueen.html", "Killer Queen"))
 songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/bohemianrhapsody.html","Bohemian Rhapsody"))
 songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/anotheronebitesthedust.html", "Another One Bites The Dust"))
+
+#Victory is just changing the function name to something barely shorter than MarkovChain.
 victory = MarkovChain()
-for song in songs:
-	i =0
-	victory.add_string(str(songs))
-	mc = victory.generate_text(50)
-	while i < 50:
-		lasso =" ".join(mc[i:i +6])
-		i +=7
-		lasso = str(lasso)
-		print lasso.replace("\\n"," ").replace("\\'","'").replace("\\r","").replace("queen lyrics","").replace("lyrics","")
+
+
+i =0
+victory.add_string(str(songs))
+mc = victory.generate_text(400)
+while i < 400:
+	lasso =" ".join(mc[i:i +6])
+	i +=7
+	lasso = str(lasso)
+	print lasso.replace("\\n"," ").replace("\\'","'").replace("\\r","").replace("queen lyrics","").replace("lyrics","")
