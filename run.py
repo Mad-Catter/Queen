efe32b1228d7b9636359b3874a7ba58766c5feb1
Mad-Catter <teriomon5@aol.com>
2018-04-06 15:00:35 -0400

    Added comments.  I am going to try to make it possible for the user to make a chain without editing code.

--- run.py
+++ run.py
@@ -1,25 +1,24 @@
 from markov_python.cc_markov import MarkovChain
 from fetch_data import cleaner
-import re
 
 songs = []
 
-cleaner("https://www.azlyrics.com/lyrics/queen/imgoingslightlymad.html","I\'m Going Slightly Mad", songs) 
-cleaner("https://www.azlyrics.com/lyrics/queen/dontstopmenow.html","Don\'t Stop Me Now", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/wewillrockyou.html", "We Will Rock You", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/sheerheartattack.html", "Sheer Heart Attack", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/youtakemybreathaway.html", "You Take My Breath Away", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/iwantitall.html","I Want It All", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/theshowmustgoon.html", "The Show Must Go On", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/killerqueen.html", "Killer Queen", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/bohemianrhapsody.html","Bohemian Rhapsody", songs)
-cleaner("https://www.azlyrics.com/lyrics/queen/anotheronebitesthedust.html", "Another One Bites The Dust", songs)
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/imgoingslightlymad.html","I\'m Going Slightly Mad")) 
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/dontstopmenow.html","Don\'t Stop Me Now"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/wewillrockyou.html", "We Will Rock You"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/sheerheartattack.html", "Sheer Heart Attack"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/youtakemybreathaway.html", "You Take My Breath Away"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/iwantitall.html","I Want It All"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/theshowmustgoon.html", "The Show Must Go On"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/killerqueen.html", "Killer Queen"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/bohemianrhapsody.html","Bohemian Rhapsody"))
+songs.append(cleaner("https://www.azlyrics.com/lyrics/queen/anotheronebitesthedust.html", "Another One Bites The Dust"))
 victory = MarkovChain()
 for song in songs:
 	i =0
 	victory.add_string(str(songs))
-	mc = victory.generate_text(360)
-	while i < 360:
+	mc = victory.generate_text(50)
+	while i < 50:
 		lasso =" ".join(mc[i:i +6])
 		i +=7
 		lasso = str(lasso)
